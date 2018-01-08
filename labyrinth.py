9e7457bacdaf52d006387c050c9fd66c9e163cbd
Apiquet <anthony.piquet2@gmail.com>
2018-01-07 23:02:23 -0800

    repeat movement if user keeps his finger on a key

--- labyrinth.py
+++ labyrinth.py
@@ -10,7 +10,7 @@ pygame.init()
 screen_size=(640,480)
 player_size_coeff=7
 player_size=(int(screen_size[0]/player_size_coeff),int(screen_size[1]/player_size_coeff))
-player_move_coeff=1
+player_move_coeff=0.1
 
 
 ##########################################################################
@@ -27,6 +27,8 @@ player_image = pygame.image.load("images/worm.jpg").convert()
 player_image.set_colorkey((255,255,255))
 #ref the player's position
 player_pos=player_image.get_rect()
+pygame.key.set_repeat(30, 30)
+
 
 continuer = 1
 
@@ -45,25 +47,25 @@ while continuer:
 		if event.type == KEYDOWN : #if user press a key
 			if event.key == K_RIGHT: #right
 				#move player position if he doesn't go outside the window
-				if player_pos.x+player_size[0]< screen_size[0]-player_size[0]:
+				if player_pos.x+player_move_coeff*player_size[0]< screen_size[0]-player_size[0]:
 					player_pos=player_pos.move(player_move_coeff*player_size[0],0)
 				else:
 					player_pos.x=screen_size[0]-player_size[0]				
 			if event.key == K_LEFT: #LEFT
 				#move player position if he doesn't go outside the window
-				if player_pos.x-player_size[0]> 0:
+				if player_pos.x-player_move_coeff*player_size[0]> 0:
 					player_pos=player_pos.move(-player_move_coeff*player_size[0],0)
 				else:
 					player_pos.x=0
 			if event.key == K_DOWN: #DOWN
 				#move player position if he doesn't go outside the window
-				if player_pos.y+player_size[1]< screen_size[1]-player_size[1]:
+				if player_pos.y+player_move_coeff*player_size[1]< screen_size[1]-player_size[1]:
 					player_pos=player_pos.move(0,player_move_coeff*player_size[1])
 				else:
 					player_pos.y=screen_size[1]-player_size[1]
 			if event.key == K_UP: #UP
 				#move player position if he doesn't go outside the window
-				if player_pos.y- player_size[1] > 0:
+				if player_pos.y-player_move_coeff*player_size[1] > 0:
 					player_pos=player_pos.move(0,-player_move_coeff*player_size[1])
 				else:
 					player_pos[1]=0
