19016b560e93c60e2d2d5a2479a59464ab689fd5
Apiquet <anthony.piquet2@gmail.com>
2018-01-07 18:39:13 -0800

    add background

--- labyrinth.py
+++ labyrinth.py
@@ -4,6 +4,20 @@ from pygame.locals import *
 #pygame initialization
 pygame.init()
 
-#Creation of the window
-fenetre = pygame.display.set_mode((640, 480))
+#creation of the window
+fenetre = pygame.display.set_mode((640,480), RESIZABLE)
 
+#background
+fond = pygame.image.load("images/background.jpg").convert()
+#adaptation of the image size to the window
+fenetre.blit(pygame.transform.scale(fond, (640, 480)), (0, 0))
+
+#display background
+pygame.display.flip()
+
+
+continuer = 1
+
+#infinite loop
+while continuer:
+	continuer = int(input()) 
