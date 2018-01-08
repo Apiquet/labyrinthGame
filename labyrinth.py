98c72430d0975e0a2ba11cf65b1c4120643c1e9c
Apiquet <anthony.piquet2@gmail.com>
2018-01-07 19:34:15 -0800

    add global variables

--- labyrinth.py
+++ labyrinth.py
@@ -4,20 +4,24 @@ from pygame.locals import *
 #pygame initialization
 pygame.init()
 
+#variables
+screen_size=(640,480)
+player_image_size=(int(screen_size[0]/10),int(screen_size[1]/10))
+
 #creation of the window
-screen = pygame.display.set_mode((640,480), RESIZABLE)
+screen = pygame.display.set_mode(screen_size, RESIZABLE)
 
 #background
 background = pygame.image.load("images/background.jpg").convert()
 #adaptation of the image size to the window
-screen.blit(pygame.transform.scale(background, (640, 480)), (0, 0))
+screen.blit(pygame.transform.scale(background, screen_size), (0, 0))
 
 #player's image
 player_image = pygame.image.load("images/worm.jpg").convert()
 #make transparent the background of the image
 player_image.set_colorkey((255,255,255))
 #adaptation of the image size to the window
-screen.blit(pygame.transform.scale(player_image, (50, 50)), (0, 0))
+screen.blit(pygame.transform.scale(player_image, (int(screen_size[0]/10),int(screen_size[1]/10))), (0, 0))
 
 #Refresh the display
 pygame.display.flip()
@@ -30,7 +34,12 @@ while continuer:
 	for event in pygame.event.get():   #verify events
 		if event.type == QUIT:     #if user want to QUIT
 			continuer = 0      #Stop the loop
-		if event.type == VIDEORESIZE:
-			screen = pygame.display.set_mode((event.w, event.h), RESIZABLE)
-			screen.blit(pygame.transform.scale(background, (event.w, event.h)), (0, 0))
+		if event.type == VIDEORESIZE:	#if user resize screen
+			#update screen size
+			screen_size=(event.w, event.h)			
+			screen = pygame.display.set_mode(screen_size, RESIZABLE)
+			#update images size
+			screen.blit(pygame.transform.scale(background, screen_size), (0, 0))
+			screen.blit(pygame.transform.scale(player_image, (int(screen_size[0]/10),int(screen_size[1]/10))), (0, 0))
+			#refresh display
 			pygame.display.flip()
