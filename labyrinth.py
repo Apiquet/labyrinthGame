51f4db593d357e04688fdc07db830ec244204296
Apiquet <anthony.piquet2@gmail.com>
2018-01-07 22:53:24 -0800

    use get_rect() attribute

--- labyrinth.py
+++ labyrinth.py
@@ -10,7 +10,6 @@ pygame.init()
 screen_size=(640,480)
 player_size_coeff=7
 player_size=(int(screen_size[0]/player_size_coeff),int(screen_size[1]/player_size_coeff))
-player_pos=(0, 0)
 player_move_coeff=1
 
 
@@ -26,6 +25,8 @@ background = pygame.image.load("images/background.jpg").convert()
 player_image = pygame.image.load("images/worm.jpg").convert()
 #make transparent the background of the image
 player_image.set_colorkey((255,255,255))
+#ref the player's position
+player_pos=player_image.get_rect()
 
 continuer = 1
 
@@ -44,28 +45,28 @@ while continuer:
 		if event.type == KEYDOWN : #if user press a key
 			if event.key == K_RIGHT: #right
 				#move player position if he doesn't go outside the window
-				if player_pos[0]+player_size[0]< screen_size[0]-player_size[0]:
-					player_pos=(player_pos[0]+player_move_coeff*player_size[0], player_pos[1])
+				if player_pos.x+player_size[0]< screen_size[0]-player_size[0]:
+					player_pos=player_pos.move(player_move_coeff*player_size[0],0)
 				else:
-					player_pos=(screen_size[0]-player_size[0], player_pos[1])				
+					player_pos.x=screen_size[0]-player_size[0]				
 			if event.key == K_LEFT: #LEFT
 				#move player position if he doesn't go outside the window
-				if player_pos[0]-player_size[0]> 0:
-					player_pos=(player_pos[0]-player_move_coeff*player_size[0], player_pos[1])
+				if player_pos.x-player_size[0]> 0:
+					player_pos=player_pos.move(-player_move_coeff*player_size[0],0)
 				else:
-					player_pos=(0, player_pos[1])
+					player_pos.x=0
 			if event.key == K_DOWN: #DOWN
 				#move player position if he doesn't go outside the window
-				if player_pos[1]+player_size[1]< screen_size[1]-player_size[1]:
-					player_pos=(player_pos[0], player_pos[1]+player_move_coeff*player_size[1])
+				if player_pos.y+player_size[1]< screen_size[1]-player_size[1]:
+					player_pos=player_pos.move(0,player_move_coeff*player_size[1])
 				else:
-					player_pos=(player_pos[0], screen_size[1]-player_size[1])
+					player_pos.y=screen_size[1]-player_size[1]
 			if event.key == K_UP: #UP
 				#move player position if he doesn't go outside the window
-				if player_pos[1]-player_size[1]> 0:
-					player_pos=(player_pos[0], player_pos[1]-player_move_coeff*player_size[1])
+				if player_pos.y- player_size[1] > 0:
+					player_pos=player_pos.move(0,-player_move_coeff*player_size[1])
 				else:
-					player_pos=(player_pos[0], 0)
+					player_pos[1]=0
 	#redisplay background to remove previous player's image
 	screen.blit(pygame.transform.scale(background, screen_size), (0, 0))
 	#display player's image on the new position
