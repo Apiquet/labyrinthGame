8ffeed74e053e4cfcc83f7efb07b5f978c63c1cd
Apiquet <anthony.piquet2@gmail.com>
2018-01-07 19:10:40 -0800

    add exit event

--- labyrinth.py
+++ labyrinth.py
@@ -5,19 +5,19 @@ from pygame.locals import *
 pygame.init()
 
 #creation of the window
-fenetre = pygame.display.set_mode((640,480), RESIZABLE)
+screen = pygame.display.set_mode((640,480), RESIZABLE)
 
 #background
-fond = pygame.image.load("images/background.jpg").convert()
+background = pygame.image.load("images/background.jpg").convert()
 #adaptation of the image size to the window
-fenetre.blit(pygame.transform.scale(fond, (640, 480)), (0, 0))
+screen.blit(pygame.transform.scale(background, (640, 480)), (0, 0))
 
 #player's image
 player_image = pygame.image.load("images/worm.jpg").convert()
 #make transparent the background of the image
 player_image.set_colorkey((255,255,255))
 #adaptation of the image size to the window
-fenetre.blit(pygame.transform.scale(player_image, (50, 50)), (0, 0))
+screen.blit(pygame.transform.scale(player_image, (50, 50)), (0, 0))
 
 #Refresh the display
 pygame.display.flip()
@@ -27,4 +27,6 @@ continuer = 1
 
 #infinite loop
 while continuer:
-	continuer = int(input()) 
+	for event in pygame.event.get():   #verify events
+		if event.type == QUIT:     #if user want to QUIT
+			continuer = 0      #Stop the loop
